954cb5cc5fe436b5f84da706c5949fc6dd8f3af9
Baya Innovation <86925273+BayaInnovation@users.noreply.github.com>
2023-04-25 02:54:03 +0300

    ሀገር በቀል እውቀት ይለምልም

---
+++ app/src/main/java/com/my/newproject23/MainActivity.java
@@ -0,0 +1,249 @@
+package com.my.newproject23;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.*;
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import com.google.android.material.appbar.AppBarLayout;
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+import android.content.*;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.webkit.*;
+import android.animation.*;
+import android.view.animation.*;
+import java.util.*;
+import java.util.regex.*;
+import java.text.*;
+import org.json.*;
+import java.util.HashMap;
+import android.widget.LinearLayout;
+import android.widget.Button;
+import android.widget.EditText;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ChildEventListener;
+import android.view.View;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.DialogFragment;
+
+
+public class MainActivity extends  AppCompatActivity  { 
+	
+	private FirebaseDatabase _firebase = FirebaseDatabase.getInstance();
+	
+	private Toolbar _toolbar;
+	private AppBarLayout _app_bar;
+	private CoordinatorLayout _coordinator;
+	private HashMap<String, Object> header = new HashMap<>();
+	private HashMap<String, Object> chapa = new HashMap<>();
+	private HashMap<String, Object> response = new HashMap<>();
+	private String handler = "";
+	private HashMap<String, Object> msl = new HashMap<>();
+	
+	private LinearLayout linear1;
+	private Button button1;
+	private EditText edittext1;
+	private LinearLayout linear2;
+	
+	private RequestNetwork cnet;
+	private RequestNetwork.RequestListener _cnet_request_listener;
+	private DatabaseReference hh = _firebase.getReference("hh");
+	private ChildEventListener _hh_child_listener;
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.main);
+		initialize(_savedInstanceState);
+		com.google.firebase.FirebaseApp.initializeApp(this);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		
+		_app_bar = (AppBarLayout) findViewById(R.id._app_bar);
+		_coordinator = (CoordinatorLayout) findViewById(R.id._coordinator);
+		_toolbar = (Toolbar) findViewById(R.id._toolbar);
+		setSupportActionBar(_toolbar);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		getSupportActionBar().setHomeButtonEnabled(true);
+		_toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _v) {
+				onBackPressed();
+			}
+		});
+		linear1 = (LinearLayout) findViewById(R.id.linear1);
+		button1 = (Button) findViewById(R.id.button1);
+		edittext1 = (EditText) findViewById(R.id.edittext1);
+		linear2 = (LinearLayout) findViewById(R.id.linear2);
+		cnet = new RequestNetwork(this);
+		
+		button1.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				header = new HashMap<>();
+				header.put("Authorization", "Bearer CHASECK_TEST-GOu5EEWv11fsJexRDhpq6HTpEQYCCPb3");
+				cnet.setHeaders(header);
+				chapa = new HashMap<>();
+				chapa.put("amount", "100");
+				chapa.put("currency", "ETB");
+				chapa.put("email", "baya@gmail.com");
+				chapa.put("first_name", "lij");
+				chapa.put("last_name", "baya");
+				chapa.put("phone_number", "0900000000");
+				chapa.put("tx_ref", hh.push().getKey());
+				chapa.put("callback_url", "https://webhook.site/077164d6-29cb-40df-ba29-8a00e59a7e60");
+				chapa.put("return_url", "https://www.google.com/");
+				chapa.put("customization[title]", "ambesa baus ticket");
+				chapa.put("customization[description]", "for travel");
+				cnet.setParams(chapa, RequestNetworkController.REQUEST_BODY);
+				cnet.startRequestNetwork(RequestNetworkController.POST, "https://api.chapa.co/v1/transaction/mobile-initialize", "", _cnet_request_listener);
+			}
+		});
+		
+		_cnet_request_listener = new RequestNetwork.RequestListener() {
+			@Override
+			public void onResponse(String _param1, String _param2, HashMap<String, Object> _param3) {
+				final String _tag = _param1;
+				final String _response = _param2;
+				final HashMap<String, Object> _responseHeaders = _param3;
+				response = new Gson().fromJson(_response, new TypeToken<HashMap<String, Object>>(){}.getType());
+				handler = (new Gson()).toJson(response.get("data"), new TypeToken<HashMap<String, Object>>(){}.getType());
+				
+				msl = new Gson().fromJson(handler, new TypeToken<HashMap<String, Object>>(){}.getType());
+				edittext1.setText(msl.get("checkout_url").toString());
+			}
+			
+			@Override
+			public void onErrorResponse(String _param1, String _param2) {
+				final String _tag = _param1;
+				final String _message = _param2;
+				
+			}
+		};
+		
+		_hh_child_listener = new ChildEventListener() {
+			@Override
+			public void onChildAdded(DataSnapshot _param1, String _param2) {
+				GenericTypeIndicator<HashMap<String, Object>> _ind = new GenericTypeIndicator<HashMap<String, Object>>() {};
+				final String _childKey = _param1.getKey();
+				final HashMap<String, Object> _childValue = _param1.getValue(_ind);
+				
+			}
+			
+			@Override
+			public void onChildChanged(DataSnapshot _param1, String _param2) {
+				GenericTypeIndicator<HashMap<String, Object>> _ind = new GenericTypeIndicator<HashMap<String, Object>>() {};
+				final String _childKey = _param1.getKey();
+				final HashMap<String, Object> _childValue = _param1.getValue(_ind);
+				
+			}
+			
+			@Override
+			public void onChildMoved(DataSnapshot _param1, String _param2) {
+				
+			}
+			
+			@Override
+			public void onChildRemoved(DataSnapshot _param1) {
+				GenericTypeIndicator<HashMap<String, Object>> _ind = new GenericTypeIndicator<HashMap<String, Object>>() {};
+				final String _childKey = _param1.getKey();
+				final HashMap<String, Object> _childValue = _param1.getValue(_ind);
+				
+			}
+			
+			@Override
+			public void onCancelled(DatabaseError _param1) {
+				final int _errorCode = _param1.getCode();
+				final String _errorMessage = _param1.getMessage();
+				
+			}
+		};
+		hh.addChildEventListener(_hh_child_listener);
+	}
+	
+	private void initializeLogic() {
+	}
+	
+	@Override
+	protected void onActivityResult(int _requestCode, int _resultCode, Intent _data) {
+		
+		super.onActivityResult(_requestCode, _resultCode, _data);
+		
+		switch (_requestCode) {
+			
+			default:
+			break;
+		}
+	}
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input){
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels(){
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels(){
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+	
+}
